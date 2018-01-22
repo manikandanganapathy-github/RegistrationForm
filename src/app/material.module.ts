40e87c40659420b496162d89aa86d9fb565da304
manikandanganapathy-github <manikandang@virtusa.com>
2018-01-22 14:07:42 +0530

    Download Options

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -5,12 +5,14 @@ import { MatButtonModule, MatToolbarModule,
      MatInputModule, MatProgressSpinnerModule, MatCardModule, MatSelectModule, MatNativeDateModule } from '@angular/material';
 import {MatRadioModule} from '@angular/material/radio';
 import {MatDatepickerModule } from '@angular/material/datepicker';
+import {MatMenuModule} from '@angular/material/menu';
 
 @NgModule({
     imports: [ MatButtonModule, MatToolbarModule, MatInputModule, MatRadioModule,
-         MatProgressSpinnerModule, MatCardModule, MatSelectModule, MatFormFieldModule, MatDatepickerModule, MatNativeDateModule],
+         MatProgressSpinnerModule, MatCardModule, MatSelectModule, MatFormFieldModule,
+         MatDatepickerModule, MatNativeDateModule, MatMenuModule],
     exports: [ MatButtonModule, MatToolbarModule, MatRadioModule,
          MatInputModule, MatProgressSpinnerModule, MatCardModule, MatSelectModule,
-          MatFormFieldModule, MatDatepickerModule, MatNativeDateModule ]
+          MatFormFieldModule, MatDatepickerModule, MatNativeDateModule, MatMenuModule ]
 })
 export class MaterialModule { }
